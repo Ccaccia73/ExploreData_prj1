c6cb9fa326ae0526e29cccba4c461be833adc52f
Claudio Caccia <c.caccia@libero.it>
2014-06-04 11:45:57 +0200

    third plot

--- Plot3.R
+++ Plot3.R
@@ -9,10 +9,10 @@ plotdata$DateTime <- paste(plotdata$Date, plotdata$Time, sep=" ")
 plotdata$Date <- as.Date(plotdata$Date, format="%d/%m/%Y")
 plotdata$DateTime <- strptime(plotdata$DateTime, format="%d/%m/%Y %H:%M:%S")
 
-#plotdata$Global_active_power <- as.numeric(as.character(plotdata$Global_active_power))
-
-
-png(filename ="plot2.png", width = 480, height = 480)
-
-plot(plotdata$DateTime, plotdata$Global_active_power, type = "l", ylab= "Global Active Power(kilowatts)", xlab="")
+# Create plot3:
+png("plot3.png", width = 480, height = 480)
+plot(plotdata$DateTime, plotdata$Sub_metering_1, type="l", ylab= "Energy sub metering", xlab="")
+lines(plotdata$DateTime, plotdata$Sub_metering_2, type="l", col="red")
+lines(plotdata$DateTime, plotdata$Sub_metering_3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, col=c("black", "red", "blue"))
 dev.off()
