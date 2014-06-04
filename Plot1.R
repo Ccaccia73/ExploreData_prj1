b4b255d767383fe3b6975245b98929e9c45dfb8c
Claudio Caccia <c.caccia@libero.it>
2014-06-04 11:47:56 +0200

    no transparent

--- Plot1.R
+++ Plot1.R
@@ -8,6 +8,6 @@ plotdata1<- subset(hpc, (hpc$Date == "1/2/2007" | hpc$Date== "2/2/2007"))
 plotdata1$Date <- as.Date(plotdata1$Date, format = "%d/%m/%Y")
 
 # build plot:
-png("plot1.png", width = 480, height = 480, bg = "transparent")
+png("plot1.png", width = 480, height = 480)
 hist(plotdata1$Global_active_power, main="Global Active Power",col='red',ylab= "Frequency", xlab="Global Active Power(kilowatts)")
 dev.off()
